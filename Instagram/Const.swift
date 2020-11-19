1499840f196d101e4f076d5a259d7a7513631143
Jumpei.Iwauchi <kiyokibarock0107@gmail.com>
2020-11-19 16:11:13 +0900

    追加しました。

---
+++ Instagram/Const.swift
@@ -0,0 +1,15 @@
+//
+//  Cost.swift
+//  Instagram
+//
+//  Created by 岩内順平 on 2020/11/19.
+//  Copyright © 2020 jumpei.iwauchi. All rights reserved.
+//
+
+import Foundation
+
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
